919193869dc80dbe558ec83aeea7ff448fcfd472
fhur <fhurtado@twilio.com>
2015-03-02 21:44:04 -0800

    Added aliases file to dotfiles

---
+++ aliases.bash
@@ -0,0 +1,13 @@
+# Aliases
+alias cs='clear'
+alias l='clear;ls -lav'
+alias '..'='cd ..'
+alias 'c'='cd'
+alias '...'='cd ../../'
+
+# Git aliases
+alias gis='clear;git status'
+alias gd='clear;git diff'
+alias gdc='clear;git diff --cached'
+
+
