79c74b1cafe0e72ec5dabf69c4440e4d76615847
fhur <fhurtado@twilio.com>
2015-04-23 10:47:40 -0500

    Added aliases/exports

--- aliases.bash
+++ aliases.bash
@@ -2,7 +2,6 @@
 alias cs='clear'
 alias l='clear;ls -lav'
 alias '..'='cd ..'
-alias 'c'='cd'
 alias '...'='cd ../../'
 
 # Git aliases
@@ -10,4 +9,17 @@ alias gis='clear;git status'
 alias gd='clear;git diff'
 alias gdc='clear;git diff --cached'
 
+# Function definitions
 
+function reload-bash(){
+  source ~/.bash_profile
+}
+
+function c(){
+  clear
+  cd $1 && ls -lav
+}
+
+function to-android(){
+  cp $1 /Users/fhurtado/twilio/authy-android/Authy/app/libs
+}
--- exports.bash
+++ exports.bash
@@ -1,2 +1,5 @@
 export ANDROID_HOME=~/Library/Android/sdk/
+export JAVA_HOME=/Library/Java/JavaVirtualMachines/jdk1.8.0_40.jdk/Contents/Home
 export PATH=$PATH:$ANDROID_HOME/tools/
+
+GIT_PS1_SHOWSTASHSTATE=true
