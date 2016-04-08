2308be98022084aa83a64c2ec512d1a62b0c6c55
Fernando <fernandohur@gmail.com>
2016-04-08 13:47:20 +0200

    added grp to aliases

--- aliases.bash
+++ aliases.bash
@@ -1,6 +1,6 @@
 # Aliases
 alias cs='clear'
-alias l='clear;ls -lv'
+alias l='clear;ls -lh'
 alias '..'='cd ..'
 alias '...'='cd ../../'
 
@@ -15,13 +15,35 @@ function reload-bash(){
   source ~/.bash_profile
 }
 
+# c DIRECTORY => changes the current dir to DIRECTORY
+# c FILE      => displays the first 40 lines in the file and the line count
 function c(){
+
   if [ -d "$1" ];
   then
     cd "$1";
     clear;
     ls -lv;
+  elif [ -f "$1" ];
+  then
+    head -n 40 "$1" && echo "Lines: " && wc -l "$1"
   else
-    head -n 40 "$1"
+    echo "$1 not found"
   fi
+}
+
+function ga(){
+  git add *$1*
+}
+
+function puts-cmd(){
+  which $1 | xargs cat
+}
+
+function xmlclean(){
+  xmllint --format $1 --output $1
+}
+
+function grp(){
+  grep -R --color "$1" .
 }
