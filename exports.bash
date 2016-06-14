2b9145a120d2b8aaaf7a444497ff01f37ac6c34b
Fernando <fernandohur@gmail.com>
2016-06-14 11:01:39 +0200

    infinite history

--- exports.bash
+++ exports.bash
@@ -1,11 +1,17 @@
-export ANDROID_HOME=~/Library/Android/sdk/
-export ANDROID_SDK=~/Library/Android/sdk/
 export JAVA_HOME=/Library/Java/JavaVirtualMachines/jdk1.8.0_51.jdk/Contents/Home
-export PATH=$PATH:$ANDROID_HOME/tools/
-export PATH=$PATH:$ANDROID_HOME/platform-tools/
-export PATH=$PATH:~/scala/bin/
-export GATLING_HOME=~/gatling/
-export GRADLE_HOME=~/gradle/
-export PATH=$PATH:$GRADLE_HOME/bin/
+
+# Eternal bash history.
+# ---------------------
+# Undocumented feature which sets the size to "unlimited".
+# http://stackoverflow.com/questions/9457233/unlimited-bash-history
+export HISTFILESIZE=
+export HISTSIZE=
+export HISTTIMEFORMAT="[%F %T] "
+# Change the file location because certain bash sessions truncate .bash_history file upon close.
+# http://superuser.com/questions/575479/bash-history-truncated-to-500-lines-on-each-login
+export HISTFILE=~/.bash_eternal_history
+# Force prompt to write history after every command.
+# http://superuser.com/questions/20900/bash-history-loss
+PROMPT_COMMAND="history -a; $PROMPT_COMMAND"
 
 GIT_PS1_SHOWSTASHSTATE=true
