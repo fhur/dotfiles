9e51482f155fb375a53b1b4482c087b37f1afaf8
Fernando Hurtado <fernandohur@gmail.com>
2016-08-14 19:29:33 +0200

    Added setup script

--- exports.bash
+++ exports.bash
@@ -1,5 +1,3 @@
-export JAVA_HOME=/Library/Java/JavaVirtualMachines/jdk1.8.0_51.jdk/Contents/Home
-
 # Eternal bash history.
 # ---------------------
 # Undocumented feature which sets the size to "unlimited".
